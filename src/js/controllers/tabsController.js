b181280a8232080ba9991381bc6cda52f31af61f
lx-PC\lx <zhnagdx890727@sina.com>
2017-01-03 20:15:34 +0800

    no message

--- src/js/controllers/tabsController.js
+++ src/js/controllers/tabsController.js
@@ -2,6 +2,24 @@
  * Created by lx on 2016/12/3.
  */
 angular.module('myApp.tabs',[]).controller('tabsController',['$scope','$ionicLoading',function ($scope,$ionicLoading) {
+    (function (doc, win) {
+        var docEl = doc.documentElement,
+            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+            recalc = function () {
+                var clientWidth = docEl.clientWidth;
+                if (!clientWidth) return;
+                if(clientWidth>=800){
+                    docEl.style.fontSize = '30px';
+                }else{
+                    docEl.style.fontSize = 10 * (clientWidth / 375) + 'px';
+                }
+            };
+
+        if (!doc.addEventListener) return;
+        win.addEventListener(resizeEvt, recalc, false);
+        doc.addEventListener('DOMContentLoaded', recalc, false);
+    })(document, window);
+
     $scope.show = function() {
         $ionicLoading.show({
             template: '<ion-spinner icon="ios"></ion-spinner>'
@@ -10,4 +28,5 @@ angular.module('myApp.tabs',[]).controller('tabsController',['$scope','$ionicLoa
     $scope.hide = function(){
         $ionicLoading.hide();
     };
+
 }]);
